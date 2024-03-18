6b8403ddb1730c7396eb24cb7de651c3b1f3a05a
Pierre <pierrechiang0210@gmail.com>
2024-03-18 13:34:23 +0800

    chore: setup dev docker compose

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,24 @@
-import { defineConfig } from "vite"
+import { defineConfig, loadEnv } from "vite"
 import react from "@vitejs/plugin-react"
 
-export default defineConfig({
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
     base: "/",
     plugins: [react()],
-    server: { port: 3000 }
+    server: {
+      host: true,
+      port: 3000,
+      watch: {
+        usePolling: true
+      },
+      proxy: {
+        "/api": {
+          target: env.BACKEND_URL,
+          changeOrigin: true
+        }
+      }
+    }
+  }
 });
